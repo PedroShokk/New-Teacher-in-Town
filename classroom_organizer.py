9c18612607e8971289357b6bab09ee8fce5660f1
PedroShokk <pribeiroeec@gmail.com>
2023-01-12 21:34:27 +0000

    group afterclass and seat combo complete

--- classroom_organizer.py
+++ classroom_organizer.py
@@ -3,39 +3,51 @@ import itertools
 
 # Import modules above this line
 class ClassroomOrganizer:
-  def __init__(self):
-    self.sorted_names = self._sort_alphabetically(student_roster)
+    def __init__(self):
+        self.sorted_names = self._sort_alphabetically(student_roster)
 
-  def _sort_alphabetically(self,students):
-    names = []
-    for student_info in students:
-      name = student_info['name']
-      names.append(name)
-    return sorted(names)
+    def _sort_alphabetically(self,students):
+        names = []
+        for student_info in students:
+            name = student_info['name']
+            names.append(name)
+        return sorted(names)
 
-  def get_students_with_subject(self, subject):
-    selected_students = []
-    for student in student_roster:
-      if student['favorite_subject'] == subject:
-        selected_students.append((student['name'], subject))
-    return selected_students
+    def get_students_with_subject(self, subject):
+        selected_students = []
+        for student in student_roster:
+            if student['favorite_subject'] == subject:
+                selected_students.append((student['name'], subject))
+        return selected_students
 
-  def __iter__(self):
-    self.index = 0 
-    return self
+    def seat_combo(self):
+        class_seats = itertools.combinations(self.sorted_names, 2)
+        return list(class_seats)
 
-  def __next__(self):
-    if self.index < len(self.sorted_names):
-      name = self.sorted_names[self.index]
-      self.index += 1
-      return name
-    else:
-      raise StopIteration
+    def __iter__(self):
+        self.index = 0 
+        return self
+
+    def __next__(self):
+        if self.index < len(self.sorted_names):
+            name = self.sorted_names[self.index]
+            self.index += 1
+            return name
+        else:
+            raise StopIteration
+
+    def afterschool_program(self):
+        likes = itertools.chain(my_class.get_students_with_subject("Math"), my_class.get_students_with_subject("Science"))
+        program_seats = itertools.combinations(likes, 4)
+        return program_seats
 
 
 my_class = ClassroomOrganizer()
 
 my_iter = iter(my_class)
 
-for i in my_iter:
-  print(i)
+#print(my_class.seat_combo())
+
+test = my_class.afterschool_program()
+for i in test:
+    print(i)
