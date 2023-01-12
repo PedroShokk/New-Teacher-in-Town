ded53d08a29690e474bf74539be0c219dc3663bf
PedroShokk <pribeiroeec@gmail.com>
2023-01-12 21:08:44 +0000

    start new project for codecademy with iters

---
+++ script.py
@@ -0,0 +1,21 @@
+import roster
+import itertools
+new_student_iter = iter(roster.student_roster)
+'''
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+print(next(new_student_iter))
+'''
+
+'''
+for i in new_student_iter:
+  print(i)
+'''
+
